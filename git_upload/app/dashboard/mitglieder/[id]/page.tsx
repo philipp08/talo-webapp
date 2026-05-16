2690f73e4e6095e3afa9fbbfb83a5c59383a0888
Philipp Pauli <pauli.philipp2020@gmail.com>
2026-05-16 21:06:32 +0200

    chore: remove redundant git_upload folder and complete sync

--- git_upload/app/dashboard/mitglieder/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import MemberDetailPage from "./MemberDetailClient";
-
-export function generateStaticParams() {
-  return [{ id: "placeholder" }];
-}
-
-export default function Page() {
-  return <MemberDetailPage />;
-}
